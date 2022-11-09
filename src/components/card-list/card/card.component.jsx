217f0c7263dfed71ea1af1e7edd3ec1696c90df7
s180608 <s180608@rguktsklm.ac.in>
2022-11-09 18:50:41 +0530

    Adding files to github pages

--- src/components/card-list/card/card.component.jsx
+++ src/components/card-list/card/card.component.jsx
@@ -6,8 +6,8 @@ export const Card = (props) => (
 
     <div className="card-container">
         <img 
-        alt="monster" 
-        src={"https://robohash.org/${props.monster.id}?set1=set2&size=180x180"}  
+         alt="monster" 
+         src={'https://robohash.org/${props.monster.id}?set=set2&size=180x180'}  
         />
         <h2> {props.monster.name}</h2>
         <p> {props.monster.email} </p>
